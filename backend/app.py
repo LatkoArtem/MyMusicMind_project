a00042c9aa6ec7651f0ef523b109ddfcc7c5d593
Latko Artem <latko8973@gmail.com>
2025-09-04 17:16:15 +0300

    fix: change SESSION_COOKIE_SECURE and SESSION_COOKIE_SAMESITE for deploy

--- backend/app.py
+++ backend/app.py
@@ -52,12 +52,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 # Session config
-# app.config['SESSION_TYPE'] = 'filesystem'
+# app.config['SESSION_TYPE'] = 'filesystem'                 <--- для локальної розробки
 # app.config['SESSION_FILE_DIR'] = './flask_session_files'  <--- для локальної розробки
 app.config['SESSION_TYPE'] = 'sqlalchemy'
 app.config['SESSION_SQLALCHEMY'] = db
-app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
-app.config['SESSION_COOKIE_SECURE'] = False
+# app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'  <--- для локальної розробки
+# app.config['SESSION_COOKIE_SECURE'] = False    <--- для локальної розробки
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_USE_SIGNER'] = True
 Session(app)
