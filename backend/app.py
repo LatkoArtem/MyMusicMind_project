22a6b001649356850e52cf5609207b7b8e675647
Latko Artem <latko8973@gmail.com>
2025-09-06 22:42:01 +0300

    fix Procfile and app.py

--- backend/app.py
+++ backend/app.py
@@ -1854,6 +1854,7 @@ def get_recommendations():
 
 # для локальної розробки
 if __name__ == "__main__":
-#     with app.app_context():
-#         db.create_all()  # Create Tables if not already present
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8888)), debug=False)
+    # with app.app_context():
+    #     db.create_all()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
